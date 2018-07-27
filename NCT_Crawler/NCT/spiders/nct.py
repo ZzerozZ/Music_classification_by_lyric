cc69819742a05e179cbede6b46c0b43031869a3c
nghiadt <nghiadt>
2018-07-27 16:53:05 +0700

    add new file

--- NCT_Crawler/NCT/spiders/nct.py
+++ NCT_Crawler/NCT/spiders/nct.py
@@ -9,15 +9,15 @@ import time
 class NCT(scrapy.Spider):
     name = 'NCT'
     def start_requests(self):
-        waitting = pd.read_csv('/home/nghia/Desktop/NCT/NCT/waitting.txt', header=None).values.ravel() 
-        crawled = pd.read_csv('/home/nghia/Desktop/NCT/NCT/crawled.txt', header=None).values.ravel()
+        waitting = pd.read_csv('/home/nghia/github/Music_classification_by_lyric/NCT_Crawler/NCT/waitting.txt', header=None).values.ravel() 
+        crawled = pd.read_csv('/home/nghia/github/Music_classification_by_lyric/NCT_Crawler/NCT/crawled.txt', header=None).values.ravel()
         for url in waitting:
             if url not in crawled:
-                open('/home/nghia/Desktop/NCT/NCT/crawled.txt', 'a+').write(url+'\n')
+                open('/home/nghia/github/Music_classification_by_lyric/NCT_Crawler/NCT/crawled.txt', 'a+').write(url+'\n')
                 yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        crawled = pd.read_csv('/home/nghia/Desktop/NCT/NCT/crawled.txt', header=None).values.ravel()
+        crawled = pd.read_csv('/home/nghia/github/Music_classification_by_lyric/NCT_Crawler/NCT/crawled.txt', header=None).values.ravel()
         with open ('Song/'+ response.url.split('/')[-1].split('.')[0]+'.txt', 'w') as f:
             # Type:
             soup = bs(response.xpath("/html[1]/body[1]/div[6]/div[2]/div[1]/div[1]/div[6]/div[1]/div[1]/b[2]/a[1]").extract_first())
@@ -31,7 +31,7 @@ class NCT(scrapy.Spider):
             links = [link.get('href') for link in links if link.get('href') not in crawled]
             links = np.unique(np.array([link for link in links if 'bai-hat' in link]))
 
-            with open('/home/nghia/Desktop/NCT/NCT/waitting.txt', 'a+') as fin:
+            with open('/home/nghia/github/Music_classification_by_lyric/NCT_Crawler/NCT/waitting.txt', 'a+') as fin:
                 for link in links:
                     fin.write(link + '\n')    
 
